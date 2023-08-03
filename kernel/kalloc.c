23567a37651b788f2a1084c7105ae24a4a113b8d
guangnianyuji <2152085@tongji.edu.cn>
2023-08-03 15:06:14 +0800

    finish lab6

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define PA2INDEX(pa) (((uint64)pa)/PGSIZE)
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -23,6 +25,8 @@ struct {
   struct run *freelist;
 } kmem;
 
+int cowcount[PHYSTOP/PGSIZE];
+
 void
 kinit()
 {
@@ -35,8 +39,12 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    
+    cowcount[PA2INDEX(p)]=1;
     kfree(p);
+  }
+    
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,6 +59,13 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&kmem.lock);
+  int remain = --cowcount[PA2INDEX(pa)];
+  release(&kmem.lock);
+
+  if(remain>0){
+    return;
+  }
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -76,7 +91,24 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    int idx = PA2INDEX(r);
+    if (cowcount[idx] != 0) {
+      panic("kalloc: cowcount[idx] != 0");
+    }
+    cowcount[idx] = 1; // 新allocate的物理页的计数器为1
+  }
+    
   return (void*)r;
+}
+
+void 
+changeref(uint64 pa,int num){
+    if (pa >= PHYSTOP) {
+        panic("addref: pa too big");
+    }
+    acquire(&kmem.lock);
+    cowcount[PA2INDEX(pa)] += num;
+    release(&kmem.lock);
 }
