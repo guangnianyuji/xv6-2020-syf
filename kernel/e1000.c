c2ec39bb235e19af9c592389fe6dd16de54d4d42
guangnianyuji <2152085@tongji.edu.cn>
2023-08-06 12:59:54 +0800

    finish lab11

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,29 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+    acquire(&e1000_lock);
+  // 查询ring里下一个packet的下标
+  int idx = regs[E1000_TDT];
+
+  if ((tx_ring[idx].status & E1000_TXD_STAT_DD) == 0) {
+    // 之前的传输还没有完成
+    release(&e1000_lock);
+    return -1;
+  }
+
+  // 释放上一个包的内存
+  if (tx_mbufs[idx])
+    mbuffree(tx_mbufs[idx]);//使用mbuffree()释放从该描述符传输的最后一个mbuf（如果有）。
+
+  // 把这个新的网络包的pointer塞到ring这个下标位置
+  tx_mbufs[idx] = m;
+  tx_ring[idx].length = m->len;
+  tx_ring[idx].addr = (uint64) m->head;
+  tx_ring[idx].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  //最后，通过将一加到E1000_TDT再对TX_RING_SIZE取模来更新环位置
+  regs[E1000_TDT] = (idx + 1) % TX_RING_SIZE;
+
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +137,23 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+ while (1) {
+    // 把所有到达的packet向上层递交
+    int idx = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+    //然后通过检查描述符status部分中的E1000_RXD_STAT_DD位来检查新数据包是否可用。如果不可用，请停止。
+    if ((rx_ring[idx].status & E1000_RXD_STAT_DD) == 0) {
+      // 没有新包了
+      return;
+    }
+    rx_mbufs[idx]->len = rx_ring[idx].length;
+    // 向上层network stack传输
+    net_rx(rx_mbufs[idx]);
+    // 把这个下标清空 放置一个空包
+    rx_mbufs[idx] = mbufalloc(0);
+    rx_ring[idx].status = 0;
+    rx_ring[idx].addr = (uint64)rx_mbufs[idx]->head;
+    regs[E1000_RDT] = idx;
+  }
 }
 
 void
